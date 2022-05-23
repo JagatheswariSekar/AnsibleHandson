9bab7103755bb9317d14139e448daa275b24536e
JagatheswariSekar <50390068+JagatheswariSekar@users.noreply.github.com>
2022-05-23 13:59:02 +0530

    Create ostype.sh

---
+++ ostype.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+case "$OSTYPE" in
+  solaris*) echo "SOLARIS" ;;
+  darwin*)  echo "OSX" ;; 
+  linux*)   echo "LINUX" ;;
+  Ubuntu*)  echo "UBUNTU";;
+  bsd*)     echo "BSD" ;;
+  msys*)    echo "WINDOWS" ;;
+  cygwin*)  echo "ALSO WINDOWS" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
